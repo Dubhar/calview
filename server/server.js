b0835f48e8fc1ef9fffe50bb4e7f108f47e17747
jero <thorsten.jerosch@soptim.de>
2023-11-22 21:00:00 -0700

    dont update on every call

--- server/server.js
+++ server/server.js
@@ -4,16 +4,27 @@ import express from 'express';
 
 const user = 'tj';
 const token = 'IMUQT-VLXJX-QTDEZ-CDBMW';
+const updateIntervalInMinutes = 15;
+const port = 5000;
 
 const app = express();
 
+let eventsByCalendar;
+let lastUpdated = new Date('2000/01/01 12:00');
+
 app.get('/events', async (request, response) => {
-    let eventsByCalendar;
-    try {
-        eventsByCalendar = await fetchEvents(user, token);
-    } catch (error) {
-        console.error(error);
+    // if needed fetch updates from server
+    let minutesSinceLastUpdate = Math.abs(new Date() - lastUpdated)/1000/60;
+    if (minutesSinceLastUpdate > updateIntervalInMinutes) {
+        try {
+            eventsByCalendar = await fetchEvents(user, token);
+            lastUpdated = new Date();
+        } catch (error) {
+            console.error(error);
+        }
     }
+
+    // convert events to json
     let allEvents = [];
     for (let calendarIndex = 0; calendarIndex < eventsByCalendar.length; ++calendarIndex) {
         const calendarColor = eventsByCalendar[calendarIndex].calendarColor;
@@ -21,7 +32,9 @@ app.get('/events', async (request, response) => {
         let convertedEvents = convertEvents(events, calendarColor);
         allEvents = allEvents.concat(convertedEvents);
     }
+
+    // send response
     response.json(allEvents);
 });
 
-app.listen(5000, () => console.log('Server started on port 5000'));
+app.listen(port, () => console.log('Server started on port '+port));
