63fb143c13df6b0d6b9b4f97f508071ebf5069c7
jero <thorsten.jerosch@soptim.de>
2023-11-22 20:00:00 -0700

    fetch/convert works

---
+++ fetch.js
@@ -0,0 +1,44 @@
+import { DAVClient } from 'tsdav';
+import("ical.js");
+
+const client = new DAVClient({
+    serverUrl: 'https://wolke.jefrickel.de/remote.php/dav/',
+    credentials: {
+        username: 'tj',
+        password: 'IMUQT-VLXJX-QTDEZ-CDBMW',
+    },
+    authMethod: 'Basic',
+    defaultAccountType: 'caldav',
+});
+
+let events = [];
+
+(async () => {
+    await client.login();
+    const calendars = await client.fetchCalendars();
+
+    for (let i=0; i<calendars.length; ++i) {
+        const calendarColor = calendars[i].calendarColor;
+        const calendarObjects = await client.fetchCalendarObjects({
+            calendar: calendars[i],
+        });
+        for (let j=0; j<calendarObjects.length; ++j) {
+            let element = calendarObjects[j].data;
+
+            let jCalElement = ICAL.parse(element);
+            let comp = new ICAL.Component(jCalElement);
+            let vevent = comp.getFirstSubcomponent("vevent");
+            let event = new ICAL.Event(vevent);
+
+            let convertedEvent = {
+                title: event.summary,
+                start: event.startDate.toJSDate(),
+                end: event.endDate.toJSDate(),
+                color: calendarColor
+            };
+            events.push(convertedEvent);
+        }
+    }
+    console.log(events);
+})();
+
